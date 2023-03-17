8c3585ceac9acb0f7db65851827fe637eca212ef
AseelAlasaad <aseelalasaad@gmail.com>
2023-03-17 19:48:59 +0000

    e-commerce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Header from './components/Header'
-import Home from './components/Home';
+import Header from './components/Header';
 function App() {
   return (
     <div>
    <Header/>
-   <Home />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,49 @@
 import React from "react";
-import "./Header.css";
-import SearchIcon from "@mui/icons-material/Search";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-function Header() {
-  return (
+import './Header.css';
+function Header()
+{
+return (
+    <>
+ 
+
     <div className="header">
       <img
         className="header_logo"
-        src="https://imgs.search.brave.com/ermYoNLzcBnu3cGzr1YuLPNZHMtoPDtWWwyPlubMcj0/rs:fit:1024:373:1/g:ce/aHR0cHM6Ly9qaXRz/dmluZ2VyLmNvLnph/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE4/LzA0L0FtYXpvbi1M/b2dvLTEwMjR4Mzcz/LnBuZw"
+        src="https://preview.colorlib.com/theme/capitalshop/assets/img/logo/logo.png.webp"
         alt="amazon"
       />
 
-      <div className="header_search">
+      {/* <div className="header_search">
         <input className="header_searchInput" type="text" />
         <SearchIcon className="header_searchIcon" />
-      </div>
+      </div> */}
 
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_optionLineOne">hello Aseel</span>
-          <span className="header_optionLineTwo">Sign In</span>
+          <span className="header_optionLineOne">Home</span>
+       
         </div>
 
         <div className="header_option">
-          <span className="header_optionLineOne">Returns</span>
-          <span className="header_optionLineTwo">& Orders</span>
+          <span className="header_optionLineOne">Shop</span>
         </div>
 
         <div className="header_option">
-          <span className="header_optionLineOne">Your</span>
-          <span className="header_optionLineTwo">Prime</span>
+          <span className="header_optionLineOne">Blog</span>
+         
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Contact</span>
         </div>
         <div className="header_optionBasket">
-           <ShoppingBasketIcon />
-           <span className="header_optionLineTwo header_basketCount">
+      
+           {/* <span className="header_optionLineTwo header_basketCount">
              0
-           </span>
+           </span> */}
         </div>
       </div>
     </div>
-  );
+    </>
+);
 }
-
 export default Header;
