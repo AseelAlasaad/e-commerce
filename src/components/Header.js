a9c86cc2674a9d61849d029f9b62b2b3b5ff4626
Aseel Alasaad <aseelalasaad@gmail.com>
2023-03-19 15:54:11 +0300

    nav and slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,81 @@
-// 
 import React from "react";
-import './Header.css'
-function Header(){
-return(
-<>
-<nav>
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Services</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</nav>
+import styled from "styled-components";
+import { Search } from "@mui/icons-material";
+import { Badge } from "@mui/material";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
 
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display:felx;
+  align-items:center;
+  justify-content: flex-end;
+`;
 
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+function Header() {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
 
-</>
-);
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 }
-export default Header
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Announcement from './Announcement'
+import Header from './Header'
+import Slider from './Slider'
+
+function Home() {
+  return (
+    <div>
+        <Announcement/>
+       <Header/>
+       <Slider/>
+    </div>
+  )
+}
+
+export default Home
