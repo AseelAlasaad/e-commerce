d017b576b08a47f125c8d55c90392dc54a9eda51
Aseel Alasaad <aseelalasaad@gmail.com>
2023-03-20 15:56:41 +0300

    Slider and arrows

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,13 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
-import React from "react";
+import React,{useState} from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
@@ -21,21 +21,82 @@ const Arrow = styled.div`
   top: 0;
   bottom: 0;
   left: ${(props) => props.direction === "left" && "10px"};
-
   right: ${(props) => props.direction === "right" && "10px"};
-
   margin: auto;
   cursor: pointer;
-  opacity:0.5;
+  opacity: 0.5;
+  z-index: 2;
 `;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  // transition:
+  transform:translateX(0vw)
+
+  
+`;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color:#${props=>props.bg}
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  height: 80%;
+  
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
 function Slider() {
+  const [slideIndex,setslideIndex]=useState(0)
+  const handleClick=(direction)=>{
+
+  }
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={()=>handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-
-      <Arrow direction="right">
+      <Wrapper>
+        <Slide bg="fsf2fd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/cXFnLLV/3.png"/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title> SUMMER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={()=>handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
